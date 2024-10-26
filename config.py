56a273fd5b441064d8dc12765a6d042174e07c7c
smitthakore <149821378+smitthakore@users.noreply.github.com>
2024-10-26 22:40:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-HF_API_KEY = "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"
+HF_API_KEY = "hf_xxxxxxxxxxxxxxxxxxxxx"
 
 HF_SUMMARIZATION_MODEL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3"
